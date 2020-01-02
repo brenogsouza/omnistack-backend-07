e270c17bfd2af6328f9f50f95a403d8cdd7e7549
brenogsouza <breno.souza.developer@gmail.com>
2020-01-02 09:32:49 -0300

    criado arquivo routes.js para definição de rotas

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const app = express();
+
 mongoose.connect('mongodb+srv://instaclone:12345@meubanco-tgxc0.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 
-app.get('/', (req, res) => {
-  return res.send('ola mundo')
-})
+app.use(require('./routes'))
 
 app.listen(3333)
