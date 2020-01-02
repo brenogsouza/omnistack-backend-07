4d8aafbad06e9fb2cfe80206788cb4e001b2cd64
brenogsouza <breno.souza.developer@gmail.com>
2020-01-02 10:11:16 -0300

    criado PostController

---
+++ src/controllers/PostController.js
@@ -0,0 +1,12 @@
+const Post = require('../models/Post')
+
+module.exports = {
+// listagem de posts
+    async index(req, res) {
+
+    },
+// criação de posts
+    async store(req, res) {
+
+    }
+}
