655f1ec65b7fda6c209a1c313d8439182b86ffeb
Breno Lopes <breno.souza.developer@gmail.com>
2020-01-02 22:36:59 -0300

    adicionando likes buscando o post do banco

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -3,7 +3,10 @@ const Post = require("../models/Post");
 module.exports = {
   // criação de likses
   async store(req, res) {
+    const post = await Post.findById(req.params.id);
+    post.likes += 1;
+    await post.save()
    
-    return res.json({ok:true});
+    return res.json(post);
   }
 };
