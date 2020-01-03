9a2e0d2deacbc4fe1c0aadcb9486a39abf75ab9f
Breno Lopes <breno.souza.developer@gmail.com>
2020-01-02 22:56:09 -0300

    disponibilizando url de files para o frontend

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 
-mongoose.connect('mongodb+srv://instaclone:12345@meubanco-tgxc0.mongodb.net/test?retryWrites=true&w=majority', {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-})
-  .then(() => console.log('Application Connected!'))
+mongoose
+  .connect(
+    "mongodb+srv://instaclone:12345@meubanco-tgxc0.mongodb.net/test?retryWrites=true&w=majority",
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    }
+  )
+  .then(() => console.log("Application Connected!"))
   .catch(err => {
     console.log(Error, err.message);
   });
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "uploads", "resized"))
+);
 
-app.use(require('./routes'))
+app.use(require("./routes"));
 
-app.listen(3333)
+app.listen(3333);
