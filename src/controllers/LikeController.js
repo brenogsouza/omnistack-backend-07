14679e09d90a9f3aa879d652f63e695663f65799
Breno Lopes <breno.souza.developer@gmail.com>
2020-01-02 22:34:47 -0300

    criado LikeController e rota para likes

---
+++ src/controllers/LikeController.js
@@ -0,0 +1,9 @@
+const Post = require("../models/Post");
+
+module.exports = {
+  // criação de likses
+  async store(req, res) {
+   
+    return res.json({ok:true});
+  }
+};
--- src/routes.js
+++ src/routes.js
@@ -8,9 +8,11 @@ const upload = multer(uploadConfig)
 
 // CONTROLLERS
 const PostController = require('./controllers/PostController')
+const LikeController = require('./controllers/LikeController')
 
 // ROTAS POST
 routes.post('/posts', upload.single('image'), PostController.store)
+routes.post('/posts/:id/likes', LikeController.store)
 
 // ROTAS GET
 routes.get('/posts', PostController.index)
